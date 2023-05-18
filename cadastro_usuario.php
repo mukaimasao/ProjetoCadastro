1fa2c894c437bcb7a14401e7bf94fbc7f41378f7
Mário <mmm.masaomukai@gmail.com>
2023-05-18 16:12:15 -0300

    termino

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -5,18 +5,29 @@
     $fone = $_POST['fone'];
     $senha = $_POST['senha'];
 
+    $comparar = "select * from usuario where email_usuario = '$email'";
+    $resulta = mysqli_query($con,$comparar);
+    $row = mysqli_num_rows($resulta);
+    if($row > 0){
+        echo"<h1>Usuário já cadastrado!</h1>";
+        echo "<a href='cadastro_usuario.html'>Voltar</a>";
+        exit();
+    }else{
+        $sql = "insert into usuario(nome_usuario, email_usuario, fone_usuario, senha_usuario)";
+        $sql .= "values ('".$nome."', '".$email."','".$fone."','".$senha."');";
+        echo $sql. "<br>";
+        $result = mysqli_query($con,$sql);
+        if($result)
+            echo "Dados cadastrados com sucesso!";
+        else 
+            echo "Erro ao tentar cadastrar!";
+    }
+
     echo "<h1>Dados do usuário</h1>";
     echo "Nome: $nome <br>";
     echo "E-mail: $email <br>";
     echo "Telefone: $fone <br>";
     echo "Senha: $senha <br>";
 
-    $sql = "insert into usuario(nome_usuario, email_usuario, fone_usuario, senha_usuario)";
-    $sql .= "values ('".$nome."', '".$email."','".$fone."','".$senha."');";
-    echo $sql. "<br>";
-    $result = mysqli_query($con,$sql);
-    if($result)
-        echo "Dados cadastrados com sucesso!";
-    else 
-        echo "Erro ao tentar cadastrar!";
+    
 ?>
